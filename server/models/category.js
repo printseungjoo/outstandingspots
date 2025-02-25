d39b7f8bed55d27ae02331dad8c4bd5826b4ef6e
printseungjoo <printseungjoo@gmail.com>
2025-02-25 06:06:48 -0500

    add

--- server/models/category.js
+++ server/models/category.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://outstanding:osforsunystudent2025@outstandingspots.xbewg.mongodb.net/category',{
+mongoose.connect('mongodb+srv://outstanding:osforsunystudent2025@outstandingspots.xbewg.mongodb.net/test/category',{
     useNewUrlParser:true,
     useUnifiedTopology:true
 })
--- server/models/option.js
+++ server/models/option.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://outstanding:osforsunystudent2025@outstandingspots.xbewg.mongodb.net/option',{
+mongoose.connect('mongodb+srv://outstanding:osforsunystudent2025@outstandingspots.xbewg.mongodb.net/test/option',{
     useNewUrlParser:true,
     useUnifiedTopology:true
 })
