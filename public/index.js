1d1a7c8683604f9ecc88f273f3ecc79f93d1b90b
printseungjoo <printseungjoo@gmail.com>
2025-02-25 02:51:18 -0500

    add

--- public/index.js
+++ public/index.js
@@ -2,10 +2,10 @@
 
 const askClick = document.getElementById('askClick');
 askClick.style.display = 'none';
-function openAsk(){
+function openAsk() {
 	askClick.style.display = 'block';
 }
-function closeAsk(){
+function closeAsk() {
 	askClick.style.display = 'none';
 }
 const adminPassword = document.getElementById('adminPassword');
@@ -14,24 +14,24 @@ const adminLoginBack1 = document.getElementById('adminLoginBack');
 adminPassword.style.display = 'none';
 adminLoginEnter.style.display = 'none';
 adminLoginBack1.style.display = 'none';
-function showPasswordBlock(){
+function showPasswordBlock() {
 	adminPassword.style.display = 'block';
 	adminLoginEnter.style.display = 'block';
 	adminLoginBack1.style.display = 'block';
 }
 
 const adminP = "osforsunystudent2025"
-function adminLogin(){
+function adminLogin() {
 	const adminInput = document.getElementById('adminPassword').value;
-	if(adminInput===adminP){
+	if (adminInput === adminP) {
 		window.location.href = 'admin.html'
 	}
-	else{
+	else {
 		alert('암호가 틀렸습니다.');
 	}
 }
 
-function adminLoginBack(){
+function adminLoginBack() {
 	const adminPassword = document.getElementById('adminPassword');
 	const adminLoginEnter = document.getElementById('adminLoginEnter');
 	const adminLoginBack1 = document.getElementById('adminLoginBack');
@@ -40,28 +40,28 @@ function adminLoginBack(){
 	adminLoginBack1.style.display = 'none';
 }
 
-async function fetchC(){
-	try{
+async function fetchC() {
+	try {
 		const response = await fetch('https://www.outstandingspots.com/category');
-		if(!response.ok){
+		if (!response.ok) {
 			throw new Error('Failed to fetch');
 		}
-		else{
+		else {
 			console.log('response is ok!');
 		}
 		const category = await response.json();
 		showCategories(category);
 	}
-	catch(err){
-		console.error('Error:',err);
+	catch (err) {
+		console.error('Error:', err);
 	}
 }
 
 let selected = JSON.parse(localStorage.getItem('selected')) || [];
-function showCategories(category){
+function showCategories(category) {
 	const categoryList = document.getElementById('categoryList');
 	categoryList.innerHTML = '';
-	category.forEach((c)=>{
+	category.forEach((c) => {
 		const div = document.createElement('div');
 		const check = document.createElement('input');
 		const label = document.createElement('label');
@@ -70,19 +70,19 @@ function showCategories(category){
 		check.value = c.name;
 		label.htmlFor = c.name;
 		label.textContent = c.name;
-		check.addEventListener('change',(e)=>{
-			if(e.target.checked){
-				if(!selected.includes(c.name)){
+		check.addEventListener('change', (e) => {
+			if (e.target.checked) {
+				if (!selected.includes(c.name)) {
 					selected.push(c.name);
 				}
 			}
-			else{
-				selected = selected.filter(value=>value!==c.name);
+			else {
+				selected = selected.filter(value => value !== c.name);
 			}
-			localStorage.setItem('selected',JSON.stringify(selected));
-			console.log('Updated',selected);
+			localStorage.setItem('selected', JSON.stringify(selected));
+			console.log('Updated', selected);
 
-			const updatePoint = new CustomEvent('checklistUpdated',{detail:selected});
+			const updatePoint = new CustomEvent('checklistUpdated', { detail: selected });
 			window.dispatchEvent(updatePoint);
 		});
 		div.appendChild(check);
@@ -91,21 +91,30 @@ function showCategories(category){
 	})
 }
 
-document.getElementById("adminPassword").addEventListener("keypress", function(e) {
-    if (e.key === "Enter") {
-        adminLogin();
-    }
+document.getElementById("adminPassword").addEventListener("keypress", function (e) {
+	if (e.key === "Enter") {
+		adminLogin();
+	}
 });
 
-document.addEventListener('DOMContentLoaded',()=>{
+document.addEventListener('DOMContentLoaded', () => {
 	console.log('I am doing(category)');
 	fetchC();
 });
 
-window.addEventListener('DOMContentLoaded', ()=>{
+window.addEventListener('DOMContentLoaded', () => {
 	localStorage.removeItem('selectedValues');
 	selected = [];
 	console.log('reloaded');
 })
 
 window.selected = selected;
+
+// document.addEventListener('DOMContentLoaded', async () => {
+// 	try {
+// 		const response = await fetch('https://ossample3.onrender.com')
+// 		const data = await response.json()
+// 	} catch (error) {
+// 		console.error('Error fetching data:', error)
+// 	}
+// })
