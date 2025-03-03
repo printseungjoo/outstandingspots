7a61bc74a58ae3ad4a2f65c442f0a71939cbf30c
printseungjoo <printseungjoo@gmail.com>
2025-03-03 08:54:30 -0500

    add

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({path:'./server/.env'});
 const express = require('express');
 const categoryModel = require('./server/models/category.js');
 const optionModel = require('./server/models/option.js');
