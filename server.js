525a76d7be7bbafdafa70b177179f76e3ed2ae60
printseungjoo <printseungjoo@gmail.com>
2025-03-04 04:44:35 -0500

    add

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(cors({
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(express.json());
-app.use('/api/auth', authRoutes);
+app.use('/api/auth', auth);
 
 app.get('/categories', async (req, res) => {
     try {
