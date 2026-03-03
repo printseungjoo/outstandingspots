9f161c8b3db29faf75e6cc893e7d98acda075c6e
Seungjoo Ham <printseungjoo@gmail.com>
2026-03-03 21:03:02 +0900

    fix: 보안 차단 에러 수정하기

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['web-production-888c9.up.railway.app'],
+    host: '0.0.0.0',
+    port: 8080
+  }
 })
