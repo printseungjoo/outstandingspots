66ae2ebbb1479c5ce14cf08b2c0a81938a9be530
Seungjoo Ham <printseungjoo@gmail.com>
2026-03-04 02:31:42 +0900

    feat: vite 설정에 경로 명시하기

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/outstandingspots/',
   plugins: [react()],
   server: {
     allowedHosts: ['web-production-888c9.up.railway.app'],
