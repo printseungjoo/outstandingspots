afd5f3a8f337195fa18663c6c6008543abba2c50
Seungjoo Ham <printseungjoo@gmail.com>
2026-03-04 02:50:36 +0900

    fix: vite 설정 경로 수정하기

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/outstandingspots/',
+  base: '/',
   plugins: [react()],
   server: {
     allowedHosts: ['web-production-888c9.up.railway.app'],
