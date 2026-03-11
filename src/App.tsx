67e43c82f58bf3d0a1fc630a1760d12bd14a49bf
Seungjoo Ham <printseungjoo@gmail.com>
2026-03-11 16:37:50 +0900

    refactor: App에서 Marker 제거하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import './App.css'
 
 import { FullMainScreen } from './ui/templates/FullMainScreen';
-import { Marker } from './ui/organisms/Marker';
 
 function App() {
   return (
     <div>
       <FullMainScreen />
-      <Marker />
     </div>
   )
 } 
