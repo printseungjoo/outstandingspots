f257d3d9e717483cb6e9bff4376a30a0e6b6511b
Seungjoo Ham <printseungjoo@gmail.com>
2026-08-14 20:56:32 +0900

    fix: 빌드 오류 수정

--- server/config/ConnectDB.ts
+++ server/config/ConnectDB.ts
@@ -4,7 +4,7 @@ export const connectDB = async (): Promise<void> => {
     try {
         await mongoose.connect(process.env.MONGO_URL as string)
         console.log('MongoDB 연결이 성공하였습니다.');
-    } catch (err) {
+    } catch {
         console.error('MongoDB에 연결할 수 없습니다.');
         process.exit(1);
     }
